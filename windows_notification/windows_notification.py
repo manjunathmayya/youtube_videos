4d9fc283244551aa540eb8bfa2f28b10e5d21305
manjunathmayya <41788269+manjunathmayya@users.noreply.github.com>
2019-09-04 00:46:48 +0530

    Create windows_notification.py

---
+++ windows_notification/windows_notification.py
@@ -0,0 +1,7 @@
+from win10toast import ToastNotifier
+
+notifier = ToastNotifier()
+
+notifier.show_toast('Title', 'Description 2 ', duration = 2)
+
+notifier.show_toast('Title', 'Description 5', duration = 5)
